4a4093778ed583f2fcb3a53e5fe7aff34f78990f
metrakon <metrakon@proton.me>
2024-03-19 20:03:03 +0200

    make build

---
+++ index.859bf1e3.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll(".gallery__panel");e.forEach(c=>{c.addEventListener("click",()=>{e.forEach(e=>{e.classList.remove("active")}),c.classList.add("active")})});
+//# sourceMappingURL=index.859bf1e3.js.map
